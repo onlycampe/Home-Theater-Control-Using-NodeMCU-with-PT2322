d9b68fd2ae875bef6795adb1b1b655370f995d9f
OnlyCampe <Rick.campe@gmail.com>
2024-08-08 09:05:30 -0400

    new update

--- HomeTheater.ino
+++ HomeTheater.ino
@@ -6,12 +6,12 @@
 #include <IRrecv.h>
 #include <IRutils.h>
 #include <SinricPro.h>
+#include <SinricProSwitch.h>
 #include <SinricProSpeaker.h>
-
 // Wi-Fi Network Credentials
 const char* ssid = "ssid";
 const char* password = "password";
-
+//
 #define RECV_PIN D4          // Pin where the IR receiver is connected
 #define PSON_PIN D8          // Power on/off pin for the Speaker
 #define BLUETOOTH_PIN D6     // Bluetooth on/off pin (controls a BC547 relay)
@@ -20,11 +20,11 @@ const char* password = "password";
 #define DEVICE_ID        "xxxxxxx"
 #define APP_KEY           "xxxxxx"
 #define APP_SECRET        "xxxxxxxxxxxxxxxxx"
-
+//
 IRrecv irrecv(RECV_PIN);      // Creates an instance of the IRrecv class for handling IR signals, using the pin defined by RECV_PIN
 decode_results results;       // Variable to store the results of decoded IR signals
-SinricProSpeaker& speaker = SinricPro[DEVICE_ID]; // Creates a reference to the SinricProSpeaker instance with the given SPEAKER_ID
-
+SinricProSpeaker& speaker = SinricPro[DEVICE_ID];
+SinricProSwitch& switchDevice = SinricPro[DEVICE_ID];
 // Variáveis de controle
 bool isSystemOn = false;
 bool isBluetoothOn = false;
@@ -32,23 +32,23 @@ bool isMuteOn = false;
 bool isDddOn = true;  
 bool isTembOn = false;
 bool loggingEnabled = false;
-
-// Declaração dos callbacks
+//callbacks
 bool onPowerState(const String &deviceId, bool &state);
-bool onSetVolume(const String &deviceId, int &volume);
-bool onAdjustVolume(const String &deviceId, int &volume, bool state);
+bool onSetVolume(const String &deviceId, int &volume, bool state);
 bool onMute(const String &deviceId, bool &state);
-
+//
+unsigned long lastVolumeUpdateTime = 0;
+const unsigned long volumeUpdateInterval = 1000; // Intervalo em milissegundos para enviar o evento após a última alteração
+int lastVolumeSent = -1; // Armazena o último volume enviado
+//
 ESP8266WebServer server(80); // Defines the web server on port 80
 PT2322 pt;                   // Creates an instance of the PT2322 class for audio control
-
 // Global variables to store volume values
 int currentTotalVol = 53;   // Initial value for Total Volume
 int currentCenterVol = 10;  // Initial value for Center Volume
 int currentSubVol = 12;     // Initial value for Subwoofer Volume
 int currentFrontVol = 10;   // Initial value for Front Volume (Left + Right)
 int currentRearVol = 10;    // Initial value for Rear Volume (Left + Right)
-
 // Global variables to store audio adjustment values
 int currentBassValue = 0;     // Initial value for Bass adjustment
 int currentMiddleValue = 0;   // Initial value for Midrange adjustment
@@ -63,7 +63,6 @@ void handleRoot() {
   String bluetoothClass = isBluetoothOn ? "green" : "red";
   String muteClass = isMuteOn ? "green" : "red";
   String dddClass = isDddOn ? "green" : "red";
-
   String html = "<!DOCTYPE html><html><head>";
   html += "<meta charset=\"UTF-8\">";
   html += "<title>Control Panel</title>";
@@ -95,7 +94,7 @@ void handleRoot() {
   html += "<button id=\"bluetoothButton\" class=\"" + bluetoothClass + "\" onclick=\"toggleButton('bluetooth')\"><i class=\"fa-brands fa-bluetooth-b fa-2x\"></i></button>";
   html += "<button id=\"muteButton\" class=\"" + muteClass + "\" onclick=\"toggleButton('mute')\"><i class=\"fas fa-volume-mute fa-2x\"></i></button>";
   html += "<button id=\"dddButton\" class=\"" + dddClass + "\" onclick=\"toggleButton('ddd')\"><i class=\"fa-solid fa-cube fa-2x\"></i></button>";
-  html += "<button id=\"audioInButton\" class=\"" + audioInClass + "\" onclick=\"toggleButton('audioIn')\"><i class=\"fas fa-music fa-2x\"></i></button>";
+  html += "<button id=\"audioInButton\" onclick=\"toggleButton('audioIn')\"><i class=\"fas fa-music fa-2x\"></i></button>";
   html += "</div>";
   html += "<div class=\"range-container\">";
   html += "<form action=\"/setVolume\" method=\"POST\">";
@@ -134,86 +133,75 @@ void handleRoot() {
   html += "}";
   html += "</script>";
   html += "</body></html>";
-
   server.send(200, "text/html", html);
 }
-// Declaração das funções
-void applySettings();
-void updateVolumeLog(int newVolume);
-
-void togglePin(int pin, bool &state, const String &onMessage, const String &offMessage) {
-  state = !state;
+void togglePin(int pin, bool state, const String &message) {
+  pinMode(pin, OUTPUT);
   digitalWrite(pin, state ? HIGH : LOW);
-  if (loggingEnabled) {
-    Serial.println(state ? onMessage : offMessage);
-  }
+  Serial.println(message);
 }
-
 bool setAudioFunction(bool &currentState, bool state, void (*setFunc)(int, int, int), int &param1, int &param2) {
   currentState = state ? 1 : 0;
   setFunc(currentMute, currentDdd, currentTemb);
-  applySettings();
   return true;
 }
-
+void updatePowerState(bool state) {
+  speaker.sendPowerStateEvent(state);
+}
+void updateVolume(int volume) {
+  unsigned long currentTime = millis();
+  lastVolumeSent = volume;
+  lastVolumeUpdateTime = currentTime;
+}
 bool onPowerState(const String &deviceId, bool &state) {
-  togglePin(PSON_PIN, isSystemOn, "System turned on", "System turned off");
+  togglePin(PSON_PIN, state, loggingEnabled ? (state ? "System turned on" : "System turned off") : "");
+  isSystemOn = state;
+  updatePowerState(state);
   return true;
 }
-
-bool onSetVolume(const String &deviceId, int &volume) {
+bool onSetVolume(const String &deviceId, int volume) {
   currentTotalVol = constrain(volume, 0, 79);
   pt.setVol(currentTotalVol);
+  updateVolume(currentTotalVol);
   if (loggingEnabled) {
-    Serial.println("Volume geral alterado");
+    Serial.printf("[Device: %s]: Volume set to %d\r\n", deviceId.c_str(), currentTotalVol);
   }
   return true;
 }
-
-bool onAdjustVolume(const String &deviceId, int &volume, bool state) {
-  currentTotalVol = constrain(currentTotalVol + volume, 0, 79);
-  pt.setVol(currentTotalVol);
+bool onMute(const String &deviceId, bool &mute) {
+  isMuteOn = mute;
+  currentMute = mute ? 1 : 0;
+  pt.setFunc(currentMute, currentDdd, currentTemb);  
   if (loggingEnabled) {
-    Serial.println("Volume geral alterado");
+    Serial.printf("[Device: %s]: Mute state changed to %s\r\n", deviceId.c_str(), mute ? "on" : "off");
   }
   return true;
 }
-
-bool onMute(const String &deviceId, bool &state) {
-  currentMute = state ? 1 : 0;
-  applySettings();
-  if (loggingEnabled) {
-    Serial.println(state ? "Mute activated" : "Mute deactivated");
-  }
-  return true;
-}
-
 bool onDdd(const String &deviceId, bool &state) {
   currentDdd = state ? 1 : 0;
-  applySettings();
+  pt.setFunc(currentMute, currentDdd, currentTemb);
   if (loggingEnabled) {
     Serial.println(state ? "DDD activated" : "DDD deactivated");
   }
   return true;
 }
-
 void handleSetFunc() {
   String func = server.arg("func");
   bool state = server.arg("state").toInt() != 0;
-
-  if (func == "bluetooth") {
-    togglePin(BLUETOOTH_PIN, isBluetoothOn, "Bluetooth turned on", "Bluetooth turned off");
-    isBluetoothOn = state;
-  } else if (func == "system") {
-    togglePin(PSON_PIN, isSystemOn, "System turned on", "System turned off");
-    isSystemOn = state;
-  } else if (func == "audioIn") {
+  if (func == "audioIn") {
     digitalWrite(AUDIO_IN_PIN, HIGH);
     delay(100);
     digitalWrite(AUDIO_IN_PIN, LOW);
     if (loggingEnabled) {
       Serial.println("Audio In pulse sent.");
     }
+  } else if (func == "bluetooth") {
+    togglePin(BLUETOOTH_PIN, state, loggingEnabled ? (state ? "Bluetooth turned on" : "Bluetooth turned off") : "");
+    isBluetoothOn = state;
+  } else if (func == "system") {
+    togglePin(PSON_PIN, state, loggingEnabled ? (state ? "System turned on" : "System turned off") : "");
+    isSystemOn = state;
+    updatePowerState(state);
   } else if (func == "mute") {
     onMute("", state);
     isMuteOn = state;
@@ -223,99 +211,106 @@ void handleSetFunc() {
   } else {
     updateAudioFunctions(func, state);
   }
-
   server.send(200, "text/plain", func + " state updated");
 }
-
 void updateAudioFunctions(const String &func, bool state) {
   if (func == "mute") {
     currentMute = state ? 1 : 0;
+    pt.setFunc(currentMute, currentDdd, currentTemb);
   } else if (func == "ddd") {
     currentDdd = state ? 1 : 0;
+    pt.setFunc(currentMute, currentDdd, currentTemb);
   }
-  applySettings();
+  if (loggingEnabled) {
+    Serial.printf("Function %s set to %s\n", func.c_str(), state ? "on" : "off");
+  }
 }
-
 void handleSetAudioSettings() {
   if (server.hasArg("bass")) {
-    int bassValue = server.arg("bass").toInt();
-    pt.setBass(bassValue);
+    currentBassValue = server.arg("bass").toInt();
+    pt.setBass(currentBassValue);
     if (loggingEnabled) {
-      Serial.println("Bass set to: " + String(bassValue));
+      Serial.println("Bass set to: " + String(currentBassValue));
     }
   }
   if (server.hasArg("middle")) {
-    int middleValue = server.arg("middle").toInt();
-    pt.setMiddle(middleValue);
+    currentMiddleValue = server.arg("middle").toInt();
+    pt.setMiddle(currentMiddleValue);
     if (loggingEnabled) {
-      Serial.println("Middle set to: " + String(middleValue));
+      Serial.println("Middle set to: " + String(currentMiddleValue));
     }
   }
   if (server.hasArg("treble")) {
-    int trebleValue = server.arg("treble").toInt();
-    pt.setTreble(trebleValue);
+    currentTrebleValue = server.arg("treble").toInt();
+    pt.setTreble(currentTrebleValue);
     if (loggingEnabled) {
-      Serial.println("Treble set to: " + String(trebleValue));
+      Serial.println("Treble set to: " + String(currentTrebleValue));
     }
   }
-  applySettings();
 }
-
 void handleSetVolume() {
   if (server.hasArg("total")) {
     currentTotalVol = constrain(server.arg("total").toInt(), 0, 79);
-    onSetVolume(DEVICE_ID, currentTotalVol);
+    pt.setVol(currentTotalVol);
+    updateVolume(currentTotalVol);
+
+    if (loggingEnabled) {
+      Serial.print("Master volume set to: ");
+      Serial.println(currentTotalVol);
+    }
   }
   if (server.hasArg("center")) {
     currentCenterVol = server.arg("center").toInt();
+    pt.setCenter_att(15 - currentCenterVol);
     if (loggingEnabled) {
-      Serial.println("Volume central alterado");
+      Serial.print("Center volume set to: ");
+      Serial.println(currentCenterVol);
     }
   }
   if (server.hasArg("sub")) {
     currentSubVol = server.arg("sub").toInt();
+    pt.setSub_att(15 - currentSubVol);
     if (loggingEnabled) {
-      Serial.println("Volume subwoofer alterado");
+      Serial.print("Subwoofer volume set to: ");
+      Serial.println(currentSubVol);
     }
   }
   if (server.hasArg("front")) {
     currentFrontVol = server.arg("front").toInt();
+    pt.setFront_lk_att(15 - currentFrontVol);
+    pt.setFront_rk_att(15 - currentFrontVol);
     if (loggingEnabled) {
-      Serial.println("Volume frontal alterado");
+      Serial.print("Front volume set to: ");
+      Serial.println(currentFrontVol);
     }
   }
   if (server.hasArg("rear")) {
     currentRearVol = server.arg("rear").toInt();
+    pt.setRear_lk_att(15 - currentRearVol);
+    pt.setRear_rk_att(15 - currentRearVol);
     if (loggingEnabled) {
-      Serial.println("Volume traseiro alterado");
+      Serial.print("Rear volume set to: ");
+      Serial.println(currentRearVol);
     }
   }
-  applySettings();
 }
-
 void processIRCommand(decode_results *results) {
-  if (results->decode_type == UNKNOWN) return;
-
-  switch (results->value) {
-    case 0xE0E0E01F:
-      if (currentTotalVol < 79) pt.setVol(++currentTotalVol);
-      if (loggingEnabled) {
-        Serial.println("Volume geral aumentado");
-      }
-      break;
-    case 0xE0E0D02F:
-      if (currentTotalVol > 0) pt.setVol(--currentTotalVol);
-      if (loggingEnabled) {
-        Serial.println("Volume geral diminuído");
-      }
-      break;
-    case 0xE0E0F00F:
-      togglePin(PSON_PIN, isSystemOn, "Speaker on", "Speaker off");
-      break;
+  if (results->decode_type != UNKNOWN) {
+    switch (results->value) {
+      case 0xE0E0E01F:
+        onSetVolume("", constrain(currentTotalVol + 1, 0, 79));
+        break;
+      case 0xE0E0D02F:
+        onSetVolume("", constrain(currentTotalVol - 1, 0, 79));
+        break;
+      case 0xE0E0F00F:
+        isSystemOn = !isSystemOn;
+        onPowerState("", isSystemOn);
+        break;
+    }
   }
   irrecv.resume();
 }
-
 void setup() {
   Serial.begin(9600);
   WiFi.begin(ssid, password);
@@ -330,24 +325,29 @@ void setup() {
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
   }
-
-  speaker.onPowerState(onPowerState);
-  speaker.onSetVolume(onSetVolume);
-  speaker.onAdjustVolume(onAdjustVolume);
-  speaker.onMute(onMute);
-
-  SinricPro.begin(APP_KEY, APP_SECRET);
-
   setupMDNS();
   setupServer();
   setupPins();
-
+  speaker.onPowerState(onPowerState);
+  speaker.onMute(onMute);
+  speaker.onSetVolume([](const String &deviceId, int &volume) -> bool {
+    return onSetVolume(deviceId, volume);
+  });
+  SinricPro.onConnected([]() {
+    if (loggingEnabled) {
+      Serial.println("[SinricPro]: Connected");
+    }
+  });
+  SinricPro.onDisconnected([]() {
+    if (loggingEnabled) {
+      Serial.println("[SinricPro]: Disconnected");
+    }
+  });SinricPro.begin(APP_KEY, APP_SECRET);
   if (loggingEnabled) {
     Serial.println("System started and ready to receive IR commands.");
   }
   applySettings();
 }
-
 void setupMDNS() {
   if (!MDNS.begin("Home")) {
     if (loggingEnabled) {
@@ -359,18 +359,15 @@ void setupMDNS() {
     Serial.println("mDNS responder started");
   }
 }
-
 void setupServer() {
   server.on("/", handleRoot);
   server.on("/setVolume", handleSetVolume);
-  server.on("/setAudioSettings", handleSetAudioSettings);
   server.on("/setFunc", HTTP_POST, handleSetFunc);
   server.begin();
   if (loggingEnabled) {
     Serial.println("HTTP server started");
   }
 }
-
 void setupPins() {
   pinMode(PSON_PIN, OUTPUT);
   pinMode(BLUETOOTH_PIN, OUTPUT);
@@ -378,30 +375,32 @@ void setupPins() {
   digitalWrite(PSON_PIN, LOW);
   digitalWrite(BLUETOOTH_PIN, LOW);
   digitalWrite(AUDIO_IN_PIN, LOW);
-  
   pinMode(D5, OUTPUT);
   digitalWrite(D5, LOW);
-  
   irrecv.enableIRIn();
   if (loggingEnabled) {
     Serial.println("IR Receiver activated");
   }
 }
-
 void loop() {
   server.handleClient();
   MDNS.update();
   SinricPro.handle();
-
   if (irrecv.decode(&results)) {
     if (loggingEnabled) {
       Serial.println("IR command received: " + String(results.value, HEX));
     }
     processIRCommand(&results);
-    applySettings();
   }
+    unsigned long currentTime = millis();
+    if (currentTime - lastVolumeUpdateTime >= volumeUpdateInterval) {
+      if (lastVolumeSent != -1) {
+        speaker.sendVolumeEvent(lastVolumeSent);
+        lastVolumeSent = -1;
+      }
+      lastVolumeUpdateTime = currentTime;
+    }
 }
-
 void applySettings() {
   pt.setVol(currentTotalVol);
   pt.setCenter_att(15 - currentCenterVol);
@@ -414,8 +413,7 @@ void applySettings() {
   pt.setBass(currentBassValue);
   pt.setMiddle(currentMiddleValue);
   pt.setTreble(currentTrebleValue);
-
   if (loggingEnabled) {
-    Serial.println("Configurações aplicadas ao PT2322:");
+    Serial.println("Configurações aplicadas ao PT2322.");
   }
 }
